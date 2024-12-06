f37f71e249e5a8e5f2e7596fcfe29b9c1283ecbe
Mohamed Soltan <soltan.mohamed@esprit.tn>
2024-12-06 14:04:43 +0100

    workshop2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { ResidencesComponent } from './residences/residences.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'products',component:ProductComponent},
+  {path:'residences',component:ResidencesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
